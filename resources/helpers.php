86aa537cf0cde113b8253e1354792bbd1662951d
Stolz <Stolz@users.noreply.github.com>
2018-03-09 23:52:36 +0800

    Added static code analysis tools

---
+++ resources/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+if ( ! function_exists('_'))
+{
+    /**
+     * Dummy gettext alias.
+     *
+     * @param  string $message
+     * @return string
+     */
+    function _($message)
+    {
+        return $message;
+    }
+}
---
+++ sami.php
@@ -0,0 +1,7 @@
+<?php
+
+return new Sami\Sami(__DIR__ . '/app', [
+    'title' => 'Laravel',
+    'build_dir' => __DIR__ . '/docs',
+    'cache_dir' => __DIR__ . '/storage/sami/cache',
+]);
