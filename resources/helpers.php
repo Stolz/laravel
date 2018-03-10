b5a2fa1e354fd19e215a492f48f254fb02c6f0dd
Stolz <Stolz@users.noreply.github.com>
2018-03-10 23:31:33 +0800

    Added new helper functions

--- resources/helpers.php
+++ resources/helpers.php
@@ -1,7 +1,6 @@
 <?php
 
-if ( ! function_exists('_'))
-{
+if (! function_exists('_')) {
     /**
      * Dummy gettext alias.
      *
@@ -11,5 +10,62 @@ if ( ! function_exists('_'))
     function _($message)
     {
         return $message;
+    }
+}
+
+if (! function_exists('d')) {
+    /**
+     * Dump the passed variables.
+     *
+     * @see http://php.net/manual/en/functions.arguments.php#functions.variable-arg-list
+     * @param  mixed  ...$args
+     * @return void
+     */
+    function d(...$args)
+    {
+        foreach ($args as $x) {
+            (new \Illuminate\Support\Debug\Dumper)->dump($x);
+        }
+    }
+}
+
+if (! function_exists('convert_to_date')) {
+    /**
+     * Convert a date value to a \Carbon\Carbon instance.
+     *
+     * Leave null values intact.
+     *
+     * @param  mixed $date
+     * @param  string $format
+     * @return \Carbon\Carbon|null
+     */
+    function convert_to_date($date, string $format = 'Y-m-d H:i:s'): ?\Carbon\Carbon
+    {
+        if (empty($date))
+            return null;
+
+        if ($date instanceof \Carbon\Carbon)
+            return $date;
+
+        return \Carbon\Carbon::createFromFormat($format, $date);
+    }
+}
+
+if (! function_exists('convert_date_to_string')) {
+    /**
+     * Convert a \Carbon\Carbon instance to string.
+     *
+     * Leave null values intact.
+     *
+     * @param  \Carbon\Carbon|null $date
+     * @param  string $format
+     * @return string|null
+     */
+    function convert_date_to_string($date, string $format = 'Y-m-d H:i:s')
+    {
+        if ($date instanceof \Carbon\Carbon)
+            return $date->format($format);
+
+        return $date;
     }
 }
