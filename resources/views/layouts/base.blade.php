f950b21f91a96535cb67d1bc0320dea53c05cb07
Stolz <Stolz@users.noreply.github.com>
2018-03-12 00:36:17 +0800

    Added base layout for all views

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <title>@yield('page-title') | {{ config('app.name') }}</title>
+
+    @hasSection('page-description')
+    <meta name="description" content="@yield('page-description')">
+    @endif
+
+    <base href="/" />
+    <meta charset="utf-8">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge,chrome=1" />{{-- Force latest IE rendering engine or Chrome Frame if available --}}
+    <meta name="author" content="twitter: @Stolz" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">{{-- Used for AJAX CSRF --}}
+    <meta name="viewport" content="width=device-width, initial-scale=1">{{-- Webkit. To disable zooming add ", maximum-scale=1, user-scalable=no" --}}
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />{{-- Favicon  --}}
+
+    {{-- CSS  --}}
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    @stack('css')
+</head>
+<body>
+    <!--[if lte IE 9]>
+    <h1>You are using an outdated browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</h1>
+    <![endif]-->
+
+    @yield('body')
+
+    {{-- JavaScript  --}}
+    <script src="{{ mix('js/app.js') }}"></script>
+    @stack('js')
+</body>
+</html>
