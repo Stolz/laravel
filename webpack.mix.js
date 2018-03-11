3f111e2bf0a065ccdb4951e297e20b5110361fc3
Stolz <Stolz@users.noreply.github.com>
2018-03-12 00:35:50 +0800

    Removed default frontend scaffolding (artisan preset none)

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,8 @@
 let mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.styles(['resources/assets/css/app.css'], 'public/css/app.css');
+mix.scripts(['resources/assets/js/app.js' ], 'public/js/app.js');
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+if (mix.inProduction()) {
+    mix.version();
+}
