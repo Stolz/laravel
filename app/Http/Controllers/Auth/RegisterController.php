f244a2fd5c285c44fe1e81c586a9d11b70c4a868
Stolz <Stolz@users.noreply.github.com>
2018-03-25 23:42:52 +0800

    Redirect to named route instead of hardcoded path

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/home'; // TODO should be route('me')
 
     /**
      * Create a new controller instance.
