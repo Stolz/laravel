59b859ec7ab5e7408cf90fffa09f31f3faab42cc
Stolz <Stolz@users.noreply.github.com>
2018-03-26 21:04:24 +0800

    Show page title in app layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
-@section('page-title', _('Home'))
-@section('page-description', _('Home page'))
+@section('page.title', _('Home page'))
 
 @section('content')
-<p>Welcome to Laravel {{ app()::VERSION }}!!</p>
+
+    <p>Welcome to Laravel {{ app()::VERSION }}!!</p>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,23 @@
 @extends('layouts.base')
 
 @section('body')
-<nav>
-    <a href="{{ route('home') }}">{{ _('Home') }}</a> |
-    @auth
-    <a href="{{ route('me') }}">{{ _('Me') }}</a> |
-    <a href="{{ route('logout') }}">{{ _('Log out') }}</a>
-    @else
-    <a href="{{ route('login') }}">{{ _('Login') }}</a>
-    @endauth
-</nav>
 
-<div id="content">
-    <h1>{{ config('app.name') }}</h1>
-    @include('flash-messages')
-    @yield('content')
-</div>
+    <nav>
+        <a href="{{ route('home') }}">{{ _('Home') }}</a> |
+        @auth
+        <a href="{{ route('me') }}">{{ _('Me') }}</a> |
+        <a href="{{ route('logout') }}">{{ _('Log out') }}</a>
+        @else
+        <a href="{{ route('login') }}">{{ _('Login') }}</a>
+        @endauth
+    </nav>
+
+    <div id="content">
+        <h1>{{ config('app.name') }} <small>@yield('page.title')</small></h1>
+
+        @include('flash-messages')
+
+        @yield('content')
+    </div>
+
 @stop
