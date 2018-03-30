966581e27a65f3b479b63d139cd0a9a89c69059f
Stolz <Stolz@users.noreply.github.com>
2018-03-30 17:31:26 +0800

    Relocate Sami docs

--- sami.php
+++ sami.php
@@ -2,6 +2,6 @@
 
 return new Sami\Sami(__DIR__ . '/app', [
     'title' => 'Laravel',
-    'build_dir' => __DIR__ . '/docs',
+    'build_dir' => __DIR__ . '/docs/sami',
     'cache_dir' => __DIR__ . '/storage/sami/cache',
 ]);
