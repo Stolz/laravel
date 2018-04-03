dfa19368413a24905718ecfadf717a7246c49e94
Stolz <Stolz@users.noreply.github.com>
2018-04-03 23:58:28 +0800

    Added Doctrine mappings for User model

---
+++ database/mappings/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Doctrine\Mappings;
+
+use LaravelDoctrine\Fluent\EntityMapping;
+use LaravelDoctrine\Fluent\Fluent;
+
+class User extends EntityMapping
+{
+    /**
+     * Returns the fully qualified name of the class that this mapper maps.
+     *
+     * @return string
+     */
+    public function mapFor()
+    {
+        return \App\Models\User::class;
+    }
+
+    /**
+     * Load the object's metadata through the Metadata Builder object.
+     *
+     * @param Fluent $builder
+     * @return void
+     */
+    public function map(Fluent $builder)
+    {
+        $builder->table('users');
+        $builder->increments('id');
+        $builder->string('name');
+        $builder->string('email')->unique();
+        $builder->string('password');
+        $builder->string('rememberToken')->length(100)->nullable();
+        $builder->carbonDateTime('createdAt')->nullable();
+        $builder->carbonDateTime('updatedAt')->nullable();
+        $builder->carbonDateTime('deletedAt')->nullable();
+    }
+}
---
+++ database/mappings/all.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    Doctrine\Mappings\User::class,
+];
