38ad572b8fb0f54bc71f4e2338323b3906dfbd20
Stolz <Stolz@users.noreply.github.com>
2018-04-03 23:59:42 +0800

    Extract logic to trait

--- app/Repositories/QueryBuilder/UserRepository.php
+++ app/Repositories/QueryBuilder/UserRepository.php
@@ -3,11 +3,12 @@
 namespace App\Repositories\QueryBuilder;
 
 use App\Repositories\Contracts\UserRepository as UserRepositoryContract;
-use Illuminate\Contracts\Auth\Authenticatable;
-use Illuminate\Support\Facades\Hash;
+use App\Repositories\Traits\UserProvider;
 
 class UserRepository extends SoftDeletableModelRepository implements UserRepositoryContract
 {
+    use UserProvider;
+
     /**
      * Database table to use.
      *
@@ -24,66 +25,5 @@ class UserRepository extends SoftDeletableModelRepository implements UserReposit
     protected function recordToModel(\StdClass $record)
     {
         return \App\Models\User::make((array) $record);
-    }
-
-    /**
-     * Retrieve a user by their unique identifier.
-     *
-     * @param  mixed  $identifier
-     * @return \Illuminate\Contracts\Auth\Authenticatable|null
-     */
-    public function retrieveById($identifier)
-    {
-        return $this->find($identifier);
-    }
-
-    /**
-     * Retrieve a user by their unique identifier and "remember me" token.
-     *
-     * @param  mixed   $identifier
-     * @param  string  $token
-     * @return \Illuminate\Contracts\Auth\Authenticatable|null
-     */
-    public function retrieveByToken($identifier, $token)
-    {
-        if ($user = $this->retrieveById($identifier) and $user->getRememberToken() and $user->getRememberToken() === $token)
-            return $user;
-
-        return null;
-    }
-
-    /**
-     * Update the "remember me" token for the given user in storage.
-     *
-     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
-     * @param  string  $token
-     * @return void
-     */
-    public function updateRememberToken(Authenticatable $user, $token)
-    {
-        $this->update($user->setRememberToken($token), [$user->getRememberTokenName()]);
-    }
-
-    /**
-     * Retrieve a user by the given credentials.
-     *
-     * @param  array  $credentials
-     * @return \Illuminate\Contracts\Auth\Authenticatable|null
-     */
-    public function retrieveByCredentials(array $credentials)
-    {
-        return $this->findBy('email', $credentials['email']);
-    }
-
-    /**
-     * Validate a user against the given credentials.
-     *
-     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
-     * @param  array  $credentials
-     * @return bool
-     */
-    public function validateCredentials(Authenticatable $user, array $credentials)
-    {
-        return $user->getEmail() === $credentials['email'] and Hash::check($credentials['password'], $user->getAuthPassword());
     }
 }
---
+++ app/Repositories/Traits/UserProvider.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Repositories\Traits;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * It is possible to implement \Illuminate\Contracts\Auth\UserProvider by only using
+ * functions from \App\Repositories\Contracts\ModelRepository and \App\Models\User.
+ * This trait implements the contract by using such functions so we don't have to repeat
+ * them for every possible implementation of \App\Repositories\Contracts\UserRepository.
+ */
+trait UserProvider
+{
+    /**
+     * Retrieve a user by their unique identifier.
+     *
+     * @param  mixed  $identifier
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveById($identifier)
+    {
+        return $this->find($identifier);
+    }
+
+    /**
+     * Retrieve a user by their unique identifier and "remember me" token.
+     *
+     * @param  mixed   $identifier
+     * @param  string  $token
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveByToken($identifier, $token)
+    {
+        if ($user = $this->retrieveById($identifier) and $user->getRememberToken() and $user->getRememberToken() === $token)
+            return $user;
+
+        return null;
+    }
+
+    /**
+     * Update the "remember me" token for the given user in storage.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  string  $token
+     * @return void
+     */
+    public function updateRememberToken(Authenticatable $user, $token)
+    {
+        $this->update($user->setRememberToken($token), [$user->getRememberTokenName()]);
+    }
+
+    /**
+     * Retrieve a user by the given credentials.
+     *
+     * @param  array  $credentials
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveByCredentials(array $credentials)
+    {
+        return $this->findBy('email', $credentials['email']);
+    }
+
+    /**
+     * Validate a user against the given credentials.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  array  $credentials
+     * @return bool
+     */
+    public function validateCredentials(Authenticatable $user, array $credentials)
+    {
+        return $user->getEmail() === $credentials['email'] and Hash::check($credentials['password'], $user->getAuthPassword());
+    }
+}
