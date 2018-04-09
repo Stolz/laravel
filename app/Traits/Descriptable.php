73624127044169e147af779b0d5c89fd283e9a3c
Stolz <Stolz@users.noreply.github.com>
2018-04-09 20:03:22 +0800

    Added trait

---
+++ app/Traits/Descriptable.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Traits;
+
+trait Descriptable
+{
+    // Meta ========================================================================
+
+    /**
+     * The description of the model.
+     *
+     * @var string|null
+     */
+    protected $description;
+
+    // Gettets =====================================================================
+
+    /**
+     * Get the description of the model.
+     *
+     * @return string|null
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    // Setters =====================================================================
+
+    /**
+     * Set the description of the model.
+     *
+     * @param  string|null $description
+     * @return self
+     */
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
