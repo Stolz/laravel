e817c31b543b8d8bdcd23e729f43fe656b718e02
Stolz <Stolz@users.noreply.github.com>
2018-06-11 18:29:25 +0800

    Use column alias

--- database/mappings/User.php
+++ database/mappings/User.php
@@ -28,7 +28,7 @@ class User extends EntityMapping
         $builder->string('name');
         $builder->string('email')->unique();
         $builder->string('password');
-        $builder->string('rememberToken')->length(100)->nullable();
+        $builder->rememberToken();
         $builder->carbonDateTime('createdAt')->nullable();
         $builder->carbonDateTime('updatedAt')->nullable();
         $builder->carbonDateTime('deletedAt')->nullable();
