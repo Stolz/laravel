a8f8ee33151b3d781e194b582bbb856d521214a1
Stolz <Stolz@users.noreply.github.com>
2018-06-20 22:38:36 +0800

    Added validation feedback to checkboxes

--- resources/views/components/form/checkbox.blade.php
+++ resources/views/components/form/checkbox.blade.php
@@ -3,11 +3,15 @@
         type="checkbox"
         id="{{ $id or $name }}"
         name="{{ $name }}"
-        class="custom-control-input {{ $class or null }}"
+        class="custom-control-input {{ $class or null }} @if($errors->has($name)) is-invalid @endif"
         @isset($checked) checked @endisset>
     <label
         for="{{ $id or $name }}"
         class="custom-control-label {{ $labelClass or null }}">
         {{ $slot }}
     </label>
+
+    @if($errors->has($name))
+        <div class="invalid-feedback">{{ $errors->first($name) }}</div>
+    @endif
 </div>
