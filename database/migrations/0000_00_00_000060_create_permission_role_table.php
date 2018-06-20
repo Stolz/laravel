278ce499dfec059790d18982c91409f652cb6581
Stolz <Stolz@users.noreply.github.com>
2018-06-20 09:26:55 +0800

    Added comments for consitency with other tables

--- database/migrations/0000_00_00_000060_create_permission_role_table.php
+++ database/migrations/0000_00_00_000060_create_permission_role_table.php
@@ -15,13 +15,14 @@ class CreatePermissionRoleTable extends Migration
     {
         Schema::create('permission_role', function (Blueprint $table) {
             $table->increments('id');
-
             $table->unsignedInteger('permission_id');
             $table->unsignedInteger('role_id');
 
+            // Foreign keys
             $table->foreign('permission_id')->references('id')->on('permissions')->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('role_id')->references('id')->on('roles')->onUpdate('cascade')->onDelete('cascade');
 
+            // Extra keys
             $table->unique(['permission_id', 'role_id']);
         });
     }
