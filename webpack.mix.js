6ca765b122b802a73d678bd68a4794b7a24f9e96
Stolz <Stolz@users.noreply.github.com>
2018-06-21 12:46:34 +0800

    Self hosts assets instead of using CDN

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,27 @@
 let mix = require('laravel-mix');
 
-mix.styles(['resources/assets/css/app.css'], 'public/css/app.css');
-mix.scripts(['resources/assets/js/app.js' ], 'public/js/app.js');
+const path = {
+    assets: 'resources/assets/',
+    bmd: 'node_modules/bootstrap-material-design/',
+    jquery: 'node_modules/jquery/',
+    popper: 'node_modules/popper.js/',
+    roboto: 'node_modules/typeface-roboto/',
+}
+
+mix.copyDirectory(path.roboto + 'files', 'public/css/files');
+
+mix.styles([
+    path.roboto + 'index.css',
+    path.bmd + 'dist/css/bootstrap-material-design.min.css',
+    path.assets + 'css/app.css',
+], 'public/css/app.css');
+
+mix.scripts([
+    path.jquery + 'dist/jquery.slim.min.js',
+    path.popper + 'dist/umd/popper.min.js',
+    path.bmd + 'dist/js/bootstrap-material-design.min.js',
+    path.assets + 'js/app.js',
+], 'public/js/app.js');
 
 if (mix.inProduction()) {
     mix.version();
