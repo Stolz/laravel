4f19f6fe24da3d1869c7cfdfa58d22fa31e5def0
Stolz <Stolz@users.noreply.github.com>
2018-06-27 11:13:17 +0800

    Fixed middleware issue when checking module permission

---
+++ app/Http/Middleware/Authorize.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authorize as Middleware;
+
+class Authorize extends Middleware
+{
+    /**
+     * Checks if the given string looks like a fully qualified class name.
+     *
+     * @param  string  $value
+     * @return bool
+     */
+    protected function isClassName($value)
+    {
+        // Class App\Providers\AuthServiceProvider defines a 'module' policy
+        // alias which the parent method misinterprets as a route model
+        // instead of a class. This work around fixes it.
+        if ($value === 'module')
+            return true;
+
+        return parent::isClassName($value);
+    }
+}
