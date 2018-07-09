361bec709842380129286890c9b4220e9fbf519f
Stolz <Stolz@users.noreply.github.com>
2018-07-09 15:36:37 +0800

    Remove unused statement

--- app/Http/Requests/User/Update.php
+++ app/Http/Requests/User/Update.php
@@ -21,8 +21,6 @@ class Update extends Create
      */
     public function rules(): array
     {
-        $minLength = \App\Models\User::MIN_PASSWORD_LENGTH;
-
         return [
             'email' => 'required|email|max:255|unique:App\Models\User,email,' . $this->user->getId(),
             'password' => 'nullable|min:' . \App\Models\User::MIN_PASSWORD_LENGTH,
