0e6f043ba78e17bc073b00fd83f8fe49e3cdb154
Stolz <Stolz@users.noreply.github.com>
2018-07-12 10:52:22 +0800

    Fix typo

--- resources/views/components/table/sortable-headers.blade.php
+++ resources/views/components/table/sortable-headers.blade.php
@@ -1,33 +1,33 @@
 <?php
-$routeParamenters = (isset($paramenters)) ? $paramenters : [];
-$buildLinks = function ($labels, $routeParamenters = []): array {
+$routeParameters = (isset($parameters)) ? $parameters : [];
+$buildLinks = function ($labels, $routeParameters = []): array {
     $route = Route::current()->getName();
     $column = request('sortBy');
     $direction = request('sortDir');
     $links = [];
 
     foreach ($labels as $key => $label) {
-        $routeParamenters['sortBy'] = $key;
+        $routeParameters['sortBy'] = $key;
 
         $class = null;
         if ($key === $column) {
             $class = 'active';
 
             if ($direction !== 'desc') {
-                $routeParamenters['sortDir'] = 'desc';
+                $routeParameters['sortDir'] = 'desc';
                 $class .= ' desc';
             } else {
                 $class .= ' asc';
             }
         }
 
-        $url = route($route, $routeParamenters);
+        $url = route($route, $routeParameters);
         $links[$key] = sprintf('<a href="%s" class="%s">%s</a>', $url, $class, e($label));
     }
 
     return $links;
 };
-$headers = $buildLinks($headers, $routeParamenters);
+$headers = $buildLinks($headers, $routeParameters);
 ?>
 
 @foreach ($headers as $header)
