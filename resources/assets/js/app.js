d7c084331449f9d990733f1bf9d9d07b479f54f9
Stolz <Stolz@users.noreply.github.com>
2018-07-16 15:35:39 +0800

    Set default AJAX headers

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
     $('body').bootstrapMaterialDesign();
 });
+
+// Default AJAX settings
+$.ajaxSetup({
+    headers: {
+        'Accept': 'application/json',
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+    }
+});
