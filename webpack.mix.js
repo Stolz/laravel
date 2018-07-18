26158f0924839bc0f4b38dc304dc09469616f6df
Stolz <Stolz@users.noreply.github.com>
2018-07-18 16:04:23 +0800

    Treat compiled assets as binary data

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 let mix = require('laravel-mix');
 
+// Common paths
 const path = {
     assets: 'resources/assets/',
     bmd: 'node_modules/bootstrap-material-design/',
@@ -7,8 +8,10 @@ const path = {
     popper: 'node_modules/popper.js/',
 }
 
+// Compile SASS into CSS
 mix.sass(path.assets + 'sass/app.scss', 'public/css/app.css');
 
+// Combine and minify vanilla JavaScript
 mix.scripts([
     path.jquery + 'dist/jquery.slim.min.js', // Do not use Slim version if you need AJAX features
     path.popper + 'dist/umd/popper.min.js',
@@ -16,6 +19,13 @@ mix.scripts([
     path.assets + 'js/app.js',
 ], 'public/js/app.js');
 
+
+// Compile webpack components
+/*mix.js([
+    path.assets + 'js/someComponent.js',
+], 'public/js/components/someComponent.js');*/
+
+// Add version to assets
 if (mix.inProduction()) {
     mix.version();
 }
