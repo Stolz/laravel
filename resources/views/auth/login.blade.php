221e32c25af37b7e169074969fbb28d5f5546b68
Stolz <Stolz@users.noreply.github.com>
2018-08-03 16:15:25 +0800

    Improved keyboard navigation for login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,26 +8,26 @@
         <form method="post" action="{{ route('login.attempt') }}" role="form" autocomplete="off">
             @csrf
 
-            @input(['type' => 'email', 'name' => 'email', 'value' => old('email'), 'attributes' => 'required autofocus maxlength=255'])
+            @input(['type' => 'email', 'name' => 'email', 'value' => old('email'), 'attributes' => 'tabindex=1 required autofocus maxlength=255'])
                 {{ _('E-Mail') }}
             @endinput
 
-            @input(['type' => 'password', 'name' => 'password', 'attributes' => 'required maxlength=255'])
+            @input(['type' => 'password', 'name' => 'password', 'attributes' => 'tabindex=1 required maxlength=255'])
                 {{ _('Password') }}
             @endinput
 
             <div class="row justify-content-between">
                 <div class="col">
-                    @checkbox(['name' => 'remember', 'checked' => old('remember')])
+                    @checkbox(['name' => 'remember', 'checked' => old('remember'), 'attributes' => 'tabindex=1'])
                         {{ _('Remember me') }}
                     @endcheckbox
                 </div>
                 <div class="col text-right">
-                    <a href="{{ route('password.request') }}"> {{ _('Forgot your password?') }}</a>
+                    <a href="{{ route('password.request') }}" tabindex="2"> {{ _('Forgot your password?') }}</a>
                 </div>
             </div>
 
-            <button type="submit" class="btn btn-outline-primary btn-block mt-4" role="button" aria-pressed="true">{{ _('Login') }}</button>
+            <button type="submit" class="btn btn-outline-primary btn-block mt-4" role="button" aria-pressed="true" tabindex="1">{{ _('Login') }}</button>
         </form>
     </div>
 </div>
