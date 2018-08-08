14c0f2707fbfd206fa9ba5f941c40df69525ad9e
Stolz <Stolz@users.noreply.github.com>
2018-08-08 14:29:40 +0800

    Update unread notifications counter via AJAX

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,28 @@
+// Default jQuery AJAX settings
+/* Not available when using Slim version of jQuery
+$.ajaxSetup({
+    headers: {
+        'Accept': 'application/json',
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+    }
+});*/
+
+// jQuery slim version lacks AJAX support so vanilla JS is used instead
+function updateUnreadNotificationsCountViaAjax(origin, destination) {
+    var ajax = new XMLHttpRequest(), count = 0;
+
+    ajax.open('GET', origin);
+    ajax.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    ajax.setRequestHeader('X-CSRF-TOKEN', $('meta[name="csrf-token"]').attr('content'));
+    ajax.onload = function() {
+        if (ajax.status === 200 && (count = parseInt(ajax.responseText)))
+            destination.text(count).show();
+        else
+            destination.text('').hide();
+    };
+    ajax.send();
+}
+
 $(document).ready(function () {
     $('body').bootstrapMaterialDesign();
 
@@ -9,12 +34,3 @@ $(document).ready(function () {
         });
     });
 });
-
-// Default AJAX settings
-/* Not available when using Slim version of jQuery
-$.ajaxSetup({
-    headers: {
-        'Accept': 'application/json',
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
-    }
-});*/
