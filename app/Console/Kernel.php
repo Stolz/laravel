bdb7ec94000c5493b35a3f8c99a4e36dcd71f6e4
Stolz <Stolz@users.noreply.github.com>
2018-08-13 09:27:38 +0800

    Added scheduled task to flush failed jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        // Purge failed jobs
+        $schedule->command('queue:flush')->daily();
     }
 
     /**
