d19186d0d9e2a7a9a4e6858df88006f35208c3c6
Stolz <Stolz@users.noreply.github.com>
2019-10-15 20:39:00 +0800

    Update policy name resolver

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -12,7 +12,7 @@ class AuthServiceProvider extends ServiceProvider
      * The policy mappings for the application.
      *
      * Only add here policies that don't follow the default naming convention.
-     * The rest will be auto-discover using `guessPolicyNamesUsing()` below.
+     * The rest will be auto-discovered using `guessPolicyNamesUsing()` below.
      *
      * @var array
      */
@@ -32,7 +32,9 @@ class AuthServiceProvider extends ServiceProvider
         // Default policy auto-discover logic looks for policies under `app\ModelName\Policies` directory. Since our models
         // have an extra namespace (app\Models\ModelName) we have to register our own logic to properly discover policies.
         Gate::guessPolicyNamesUsing(function ($modelClass) {
-            return ['App\\Policies\\' . class_basename($modelClass) . 'Policy'];
+            $policy = studly_case(class_basename($modelClass));
+
+            return ["App\Policies\\{$policy}Policy"];
         });
 
         // Register our own user provider for authentication
